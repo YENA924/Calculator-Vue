fb512a2df2cb77f18348284532a9ba6b637736f1
yena <wyena924@gmail.com>
2022-03-29 14:28:41 +0900

    - vue cli eletron builder 플러그인 설치

---
+++ src/components/Calculator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="calculator">
+    <div class="calc__header">
+      <div class="calc__history"></div>
+      <div class="calc__input_container"></div>
+    </div>
+    <div class="calc__contents">
+      <div class="calc__keypad"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
