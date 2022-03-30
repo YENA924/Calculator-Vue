cbee75ced03550b300964f49f4999008a959defb
yena <wyena924@gmail.com>
2022-03-30 15:37:37 +0900

    - css import 수정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 
 module.exports = {
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/style/main.scss";`
+      }
+    }
+  },
   transpileDependencies: true
 }
